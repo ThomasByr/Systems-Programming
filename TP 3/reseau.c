cf254eb3025619504a53ca94d2b76978f8de4ddd
ThomasByr <tbouyer2000@gmail.com>
2022-02-26 15:42:50 +0100

    wp 3 (currently unrevised)

--- TP 3/reseau.c
+++ TP 3/reseau.c
@@ -40,6 +40,55 @@ noreturn void alert(int syserr, const char *msg, ...) {
     exit(EXIT_FAILURE);
 }
 
+struct sta_s {
+    int dest;
+    char payload[PAYLOAD_SIZE];
+};
+
+struct info_s {
+    int src;
+    int dest;
+    char payload[PAYLOAD_SIZE];
+};
+
+void child_main(int id, int in, int out) {
+    int fd, n;
+    char filename[10];
+    struct sta_s sta;
+    struct info_s info;
+
+    sprintf(filename, "STA_%d", id);
+    CHK((fd = open(filename, O_RDONLY)));
+
+    // format : (dest payload)
+    while ((n = read(fd, &sta, sizeof(sta))) > 0) {
+        info.src = id;
+        info.dest = sta.dest;
+        strncpy(info.payload, sta.payload, PAYLOAD_SIZE);
+
+        // send (dest payload) to parent via pipe
+        CHK(write(out, &info, sizeof(info)));
+    }
+    if (n == -1)
+        alert(1, "read");
+
+    CHK(close(fd));
+    CHK(close(out));
+
+    while ((n = read(in, &info, sizeof(info))) > 0) {
+        // wait for parent to send back (src dest payload)
+        // print (id - src - dest - payload)
+        fprintf(stdout, "%d - %d - %d - ", id, info.src, info.dest);
+        fwrite(info.payload, STDOUT_FILENO, PAYLOAD_SIZE, stdout);
+        fprintf(stdout, "\n");
+        fflush(stdout);
+    }
+    if (n == -1)
+        alert(1, "read");
+
+    CHK(close(in));
+}
+
 int main(int argc, char *argv[]) {
     long no_sta; // number of stations
 
@@ -59,7 +108,7 @@ int main(int argc, char *argv[]) {
     int pipes[MAXSTA + 1][2];
     for (long i = 0; i < no_sta + 1; i++) {
         CHK(pipe(pipes[i]));
-    }
+    } // todo: do not create to many useless pipes
 
     for (long i = 1; i < no_sta + 1; i++) {
         switch (fork()) {
@@ -78,6 +127,8 @@ int main(int argc, char *argv[]) {
                 }
             }
 
+            child_main(i, pipes[i][0], pipes[0][1]);
+
             exit(EXIT_SUCCESS);
         }
     }
@@ -88,5 +139,40 @@ int main(int argc, char *argv[]) {
         CHK(close(pipes[i][0]));
     }
 
-    printf("%ld\n", no_sta);
+    // read no_sta (src dest payload) from all children
+    struct info_s info;
+    int n;
+    while ((n = read(pipes[0][0], &info, sizeof(info))) > 0) {
+
+        // send (src dest payload) dest children via pipes
+        // if dest is undefined, send to all children except src
+
+        if (1 <= info.dest && info.dest <= no_sta) {
+            CHK(write(pipes[info.dest][1], &info, sizeof(info)));
+        } else {
+            for (long j = 1; j < no_sta + 1; j++) {
+                if (j != info.src) {
+                    CHK(write(pipes[j][1], &info, sizeof(info)));
+                }
+            }
+        }
+    }
+    if (n == -1)
+        alert(1, "read");
+
+    CHK(close(pipes[0][0]));
+    for (long i = 1; i < no_sta + 1; i++) {
+        CHK(close(pipes[i][1]));
+    }
+
+    // wait for all children
+    int status, exit_status = EXIT_SUCCESS;
+    for (long i = 1; i < no_sta + 1; i++) {
+        CHK(wait(&status));
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS) {
+            exit_status = EXIT_FAILURE;
+        }
+    }
+    return exit_status;
 }
